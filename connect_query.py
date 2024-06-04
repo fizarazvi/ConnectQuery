05624ea56f2bb7b94121fb6dffd88936865a1083
fizamayfair <fiza.razvi@mayfairvillage.io>
2024-06-04 07:46:08 -0400

    format the chain output using Pydantic

--- connect_query.py
+++ connect_query.py
@@ -5,6 +5,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 from agents.linkedin_lookup_agent import lookup
 from third_parties.linkedin import scrape_linkedin_data
+from output_parsers import summary_parser
 
 import os
 import sys
@@ -16,19 +17,22 @@ def connect_query(name: str):
         given the information {information} about a person I want you to create:
         1. Short summary
         2. two interesting fact about them
+        \n {format_instructions}
     """
 
     # information should be passed to the chain, here we just provide what vairable to look for
-    summary_prompt_template = PromptTemplate(input_variables=['information'], template=summary_template)
+    summary_prompt_template = PromptTemplate(input_variables=['information'], 
+                                             template=summary_template, 
+                                             partial_variables={"format_instructions":summary_parser.get_format_instructions()})
 
     #temperature defines the creativity of the mode
     llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo')
 
-    chain = summary_prompt_template | llm
+    chain = summary_prompt_template | llm | summary_parser
 
-    res = chain.invoke(input={"information":information})
+    res = chain.invoke(input={"information":information}, )
 
-    print(res.content)
+    print(res)
 
 
 
